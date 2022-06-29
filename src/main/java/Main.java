b00091107424fd3bcaf8b9d8e3e9f572261ca08d
todd-bowers <toddbows@gmail.com>
2022-06-28 21:39:10 -0400

    Done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+    Scanner in = new Scanner(System.in);
+    System.out.print("What is your name? ");
+    String name = in.nextLine();
 
+    switch (name) {
+        case "Alice":
+            System.out.println("Hello Alice!");
+            break;
+        case "Bob":
+            System.out.println("Hello Bob!");
+            break;
     }
-}
+    }
+    }
